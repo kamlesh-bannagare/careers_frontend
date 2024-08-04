ef65e82ebda3955aca9f73a92ce973291094f551
kamlesh-bannagare <kamleshbannagare10@gmail.com>
2024-08-04 15:41:35 +0530

    added proper skeleton

--- careers_ui/src/components/common/Loading/Loading.jsx
+++ careers_ui/src/components/common/Loading/Loading.jsx
@@ -3,66 +3,68 @@ import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
 import Skeleton from '@mui/material/Skeleton';
+import {
+  useMediaQuery,
+} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-const Image = styled('img')({
+const ImageSkeleton = styled(Skeleton)({
   width: '100%',
+  height: '100%',
+  borderRadius: 'inherit', // Matches border radius of actual images
+  objectFit: 'cover',
 });
 
-function SkeletonChildrenDemo(props) {
-  const { loading = false } = props;
-
+function SkeletonChildrenDemo({ cols = 1, rows = 1 }) {
   return (
-    <div>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Box sx={{ margin: 1 }}>
-          {loading ? (
-            <Skeleton variant="circular">
-              <Avatar />
-            </Skeleton>
-          ) : (
-            <Avatar src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg" />
-          )}
-        </Box>
-        <Box sx={{ width: '100%' }}>
-          {loading ? (
-            <Skeleton width="100%">
-              <Typography>.</Typography>
-            </Skeleton>
-          ) : (
-            <Typography>Ted</Typography>
-          )}
-        </Box>
+    <Box
+      sx={{
+        borderRadius: 2,
+        overflow: 'hidden',
+        position: 'relative',
+        mb: 4,
+        width: '100%',
+        height: 'auto',
+      }}
+    >
+      <ImageSkeleton variant="rectangular" width="100%" height="200px" />
+      <Box sx={{ p: 1 }}>
+        <Skeleton variant="text" width="60%" />
+        <Skeleton variant="text" width="80%" />
       </Box>
-      {loading ? (
-        <Skeleton variant="rectangular" width="100%">
-          <div style={{ paddingTop: '57%' }} />
-        </Skeleton>
-      ) : (
-        <Image
-          src="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
-          alt=""
-        />
-      )}
-    </div>
+    </Box>
   );
 }
 
 SkeletonChildrenDemo.propTypes = {
-  loading: PropTypes.bool,
+  cols: PropTypes.number,
+  rows: PropTypes.number,
 };
 
 export default function SkeletonChildren() {
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.only('xs'));
+  const isSm = useMediaQuery(theme.breakpoints.only('sm'));
+  const isMd = useMediaQuery(theme.breakpoints.only('md'));
+
+  const getCols = () => {
+    if (isXs) return 1;
+    if (isSm) return 1;
+    if (isMd) return 3;
+    return 4;
+  };
+
   return (
-    <Grid container spacing={8}>
-      <Grid item xs>
-        <SkeletonChildrenDemo loading />
+    <Box sx={{ width: '100%', height: 'auto', overflowY: 'auto', p: 2 }}>
+      <Grid container spacing={2}>
+        {Array.from(new Array(12)).map((_, index) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+            <SkeletonChildrenDemo />
+          </Grid>
+        ))}
       </Grid>
-      <Grid item xs>
-        <SkeletonChildrenDemo loading/>
-      </Grid>
-    </Grid>
+    </Box>
   );
 }
